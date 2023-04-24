b52efdd469641bacb47b76173b74536ca439a2f5
ideyedi <gdregas18@gmail.com>
2023-04-24 12:43:19 +0900

    config 추가

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.version import __version__ as proj_version
+
+app = FastAPI(
+    title="ph-server",
+    version=proj_version,
+    docs_url="/",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.include_router()
